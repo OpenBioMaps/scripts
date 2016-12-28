925b26763d04b13216c5ff07a4d269d52c67bcc0
Bán Miklós <banm@vocs.unideb.hu>
2016-12-28 01:11:41 +0100

    Add files via upload
    I have extended the normal archive with a more complex table choice solution:
    Now it using a parameter file which should contains line by line the table names. Only the project table and the system table names.
    This file also should contain the archive plan in cron like syntax.
    Added a small extension on full archive: optional database name after the "full" parameter.
    I have eliminated the tables' archives from the full archive

--- obm_archive.sh
+++ obm_archive.sh
@@ -4,8 +4,7 @@
 #2016-10-31
 #feel free to upgrade it!
 #please share your improvements:
-#administrator@lists.openbiomaps.org
-#https://github.com/OpenBioMaps/archive_scripts
+#administrator@openbiomaps.org
 
 # crontab usage examples:
 # only tables from Monday to Saturday
@@ -15,10 +14,27 @@
 
 # Variables - set them as you need
 date=`date +"%b-%d-%y_%H:%M"`
+# cron like archive sttings
+doweek=`date +"%d"`
+month=`date +"%m"`
+day=`date +"%u"`
+# path of table list
+table_list="${HOME}/.archive_list.txt"
+#table dayof_week dayofmonth month
+#foo at every day
+#foo * * *
+#bar every Monday
+#bar 1 * *
+#casbla at every 1st day of every June
+#casbla * 1 6
+
+
 # tables in gisdata
-tables=(templates templates_genetics templates_taxon files file_connect)
+special_tables=(uploadings files file_connect)
+#tables=( $(cat $table_list) )
 dbs=(gisdata biomaps)
 archive_path="/home/archives"
+tables=()
 
 # Remote 
 remote_user=''
@@ -28,24 +44,56 @@ remote_path=''
 case "$1" in
 normal) echo "dumping tables"
 
-    for i in "${tables[@]}"
+    while read i
+    do
+        [[ $i == "#"* ]] && continue
+        IFS=' ' read -r -a sa <<< "$i"
+        crd=${sa[1]} #day
+        crw=${sa[2]} #week
+        crm=${sa[3]} #month
+        table=${sa[0]}
+        #crony
+        if [[ "$crm" == "*" || "$crm" == "$month" ]]; then
+            if [[ "$crw" == "*" || "$crw" == "$doweek" ]]; then
+                if [[ "$crd" == "*" || "$crd" == "$day" ]]; then
+                    if ! echo ${special_tables[@]} | grep -q -w "$table"; then 
+                        # normal tables            
+                        mt=$(echo "SELECT array_to_string(main_table,';') as t FROM projects WHERE project_table='$table'" | psql -t -h localhost -U gisadmin biomaps)
+                        if [ -z "$mt" ]; then
+                            echo "Unknown project: $table"
+                        else
+                            main_tables=(${mt//;/ })
+                            tables+=( "${main_tables[@]}" )
+                            # automatically add history and taxon tables
+                            # probably some customization would be nice
+                            tables+=( `printf "%s_history %s_taxon" $table $table` )
+                        fi
+                    else
+                        tables+=( $table )
+                    fi
+                fi
+            fi
+        fi
+    done < $table_list
+
+    #run
+    for k in "${tables[@]}"
     do 
-        printf "pg_dump -U gisadmin -f %s/gisdata_%s_%s.sql -n public -t %s gisdata\n" $archive_path $i $date $i | bash
+        printf "pg_dump -U gisadmin -f %s/gisdata_%s_%s.sql -n public -t %s gisdata" $archive_path $k $date $k | bash
+        #printf "pg_dump -U gisadmin -f %s/gisdata_%s_%s.sql -n public -t %s gisdata\n" $archive_path $k $date $k
     done
 
 echo "."
 ;;
-full) echo "dumping tables and databases"
+full) echo "dumping databases"
     
-    echo "tables"
-    for i in "${tables[@]}"
-    do 
-        printf "pg_dump -U gisadmin -f %s/gisdata_%s_%s.sql -n public -t %s gisdata" $archive_path $i $date $i | bash
-    done
-
-    echo "databases"
     for i in "${dbs[@]}"
     do 
+        if [ $# -eq 2 ] && [ $i != $2 ]; then 
+            continue
+        fi
+        echo $i
+        #printf "pg_dump -U gisadmin -f %s/%s_%s.sql -n public %s\n" $archive_path $i $date $i
         printf "pg_dump -U gisadmin -f %s/%s_%s.sql -n public %s" $archive_path $i $date $i | bash
     done
 
@@ -75,7 +123,10 @@ clean) echo "cleaning: gzipping sql files and deleting old gzip files"
         n1="$2"
         n2="$3"
     fi
+    # gzipping everything older than 7 days
     printf "find %s -type f -name '*.sql' -mtime +$n1 -print -exec gzip {} \;" $archive_path | bash
+    # delete every gzip file older than 30 days
+    # how can I keep 1/month?
     printf "find %s -type f -name '*.gz' -mtime +$n2 -print -exec rm {} \;" $archive_path | bash
 
 echo "."
