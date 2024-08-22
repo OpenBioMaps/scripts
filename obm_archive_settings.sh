d3d322fae49537384f5c4bc04152e9afaebae173
Bán Miklós <banm@vocs.unideb.hu>
2024-08-22 15:18:38 +0200

    Update obm_archive_settings.sh

--- obm_archive_settings.sh
+++ obm_archive_settings.sh
@@ -4,6 +4,7 @@ table_list="${HOME}/.archive_list.txt"
 # postgres parameters
 project_database="gisdata"
 system_database="biomaps"
+all_databases="gisdata"
 admin_user="gisadmin"
 archive_path="/home/archives"
 pgport="5432"
@@ -11,7 +12,7 @@ pg_dump="pg_dump -p $pgport"
 psql="psql -p $pgport"
 
 # FOR DOCKER based OBM systems
-# docker="/usr/bin/docker-compose -f /path/to/docker-compose.yml exec -T"
+# docker="/usr/bin/docker-compose -f /PATH/TO/docker-compose.yml exec -T"
 # pg_dump="$docker biomaps_db pg_dump -p $pgport"
 # psql="$docker biomaps_db psql -p $pgport"
 
