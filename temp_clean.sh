604429758cd490ae6a4dd46b9173e7071e0e91f6
Bán Miklós <banm@vocs.unideb.hu>
2026-06-25 10:59:13 +0200

    Update temp_clean.sh

--- temp_clean.sh
+++ temp_clean.sh
@@ -2,22 +2,31 @@
 
 DB=$1
 
+# Host PostgreSQL
+# PSQL="psql -U biomaps_admin -h localhost -At -d $DB"
+
+# Docker PostgreSQL
+PSQL="docker-compose exec -T biomaps_db psql -U biomapsadmin -At $DB"
+
 while true; do
-  deleted=$(psql -U biomaps_admin -h localhost -At -d $DB -c "
-    DELETE FROM system.temp_index
-    WHERE table_name IN (
-      SELECT table_name
-      FROM system.temp_index ti
-      LEFT JOIN system.imports i ON ti.table_name = i.file
-      WHERE i.project_table IS NULL
-        AND ti.datum < NOW() - INTERVAL '1 day'
-      LIMIT 500
-    )
-    RETURNING 1;" | wc -l)
+  deleted=$(
+    $PSQL -c "
+      DELETE FROM system.temp_index
+      WHERE table_name IN (
+        SELECT table_name
+        FROM system.temp_index ti
+        LEFT JOIN system.imports i ON ti.table_name = i.file
+        WHERE i.project_table IS NULL
+          AND ti.datum < NOW() - INTERVAL '1 day'
+        LIMIT 500
+      )
+      RETURNING 1;
+    " | wc -l
+  )
 
   echo "Törölve: $deleted sor"
+
   [ "$deleted" -le 1 ] && break
 
   sleep 0.1
 done
-
